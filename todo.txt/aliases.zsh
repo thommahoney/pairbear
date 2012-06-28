3d4bf8c177ef4a1a5461c3ab77b36ec84fb7de4e
alieander <tommy@huddler-inc.com>
2012-06-28 23:15:51 +0000

    using linode as todo server... fun!

--- todo.txt/aliases.zsh
+++ todo.txt/aliases.zsh
@@ -1,9 +1,9 @@
 # todo.sh: https://github.com/ginatrapani/todo.txt-cli
 function t() { 
   if [ $# -eq 0 ]; then
-    todo.sh ls
+    ssh -qt tommy@goatorboat.com "/usr/local/todo.txt-cli/todo.sh ls"
   else
-    todo.sh $*
+    ssh -qt tommy@goatorboat.com "/usr/local/todo.txt-cli/todo.sh $*"
   fi
 }
 
