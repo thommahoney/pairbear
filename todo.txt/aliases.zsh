f51861b44ef26c898b867e74049cbc4853c16f0d
Tommy O'Neil <alieander@gmail.com>
2013-06-13 14:29:04 -0700

    updates to todo helper

--- todo.txt/aliases.zsh
+++ todo.txt/aliases.zsh
@@ -1,18 +1,22 @@
 # todo.sh: https://github.com/ginatrapani/todo.txt-cli
 function t() { 
-  if [ $# -eq 0 ]; then
-    ssh -qt tommy@goatorboat.com "/usr/local/todo.txt-cli/todo.sh ls"
-  else
-    ssh -qt tommy@goatorboat.com "/usr/local/todo.txt-cli/todo.sh $*"
-  fi
+	if [ $# -eq 0 ]; then
+		ssh -qt tommy@goatorboat.com "/usr/local/todo.txt-cli/todo.sh ls"
+	else
+		VAR=$1
+		shift
+		ssh -qt tommy@goatorboat.com "/usr/local/todo.txt-cli/todo.sh $VAR \"$*\" "
+	fi
 }
 
 function tn() {
-  todo.sh app $1 +next
+	t app $1 +next
 }
 
+
 alias n="t ls +next"
+alias tt="t ls +tix"
 alias td="t do "
 alias ta="t ls '(A)'"
 alias tb="t ls '(B)'"
-alias tv="t ls '(C)'"
+alias tc="t ls '(C)'"
