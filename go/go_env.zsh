cf75a1ad53dee3d94adb8bf01738a62aae21146b
Tommy O'Neil <alieander@gmail.com>
2013-08-28 13:15:34 -0700

    Add Vim-Go stuff and improving go_env

---
+++ go/go_env.zsh
@@ -0,0 +1,13 @@
+case $OSTYPE in
+*"darwin"*)
+	export GOVERSION="1.1"
+	export GOROOT="$(brew --prefix)"/Cellar/go/$GOVERSION
+	export GOPATH=$PROJECTS/gocode
+	export PATH=$PATH:$GOROOT/bin:${GOPATH//://bin:}/bin
+	;;
+*"linux"*)
+	export GOPATH=$HOME/workspace/go
+	export GOROOT=/usr/local/go
+	export PATH=$PATH:$GOROOT/bin:${GOPATH//://bin:}/bin
+	;;
+esac
