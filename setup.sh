6226d8dd5ec5839fb31a95f944ebc04b416cf8c4
alieander <alieander@gmail.com>
2013-10-28 07:33:54 -0700

    list of things that should be in a setup/install script

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+#1. check for needed things like {xcode cm tools, homebrew, etc}
+#2. install the things that we can from cli
+#3. downloadd things that I can from cli
+#4. run git submodule init and git submodule update
+#5. setup remainder of git's global config file
+
+#6. mkdir working directory structures
+#7. generate a list of projects that I always clone (perhaps not into projects directly)
+#8. 
+
+
+#possible brew installs
+	#:python
+	#:git
+
+#possible git repos for new mac
+	#:slate
+	#:powerline
+	#:powerline-fonts
+	#:geeknote
+
+
+
+#git clone git://github.com/VitaliyRodnenko/geeknote.git
+#geeknotes.py login
+
+
+#cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
+
