daefccfbba4b2287262afde53daa93f46a9fb496
Tommy O'Neil <alieander@gmail.com>
2014-01-30 12:15:51 -0800

    pauper helpers

--- fastly/functions.zsh
+++ fastly/functions.zsh
@@ -1,1 +1,14 @@
+VERSION='0.2.0'
+pauper_up() {
+	ssh -t tommy@turret.local 'cd /home/tommy/fastly/pauper-env; ./start.sh';
+	cd /Users/tommy/Projects/fastly/pauper-env;
+	pauper setup_osx;
+}
+
+pauper_install() {
+	cd /Users/tommy/Projects/fastly/Pauper;
+	sudo gem build pauper.gemspec;
+	sudo gem install pauper-$VERSION.gem;
+	ssh -t tommy@turret.local "cd /home/tommy/fastly/Pauper; sudo gem install pauper-$VERSION.gem";
+}
 
