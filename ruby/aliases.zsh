072a1fce7bdb0861bebfe8e170dbb02b4ad8f47d
Tommy O'Neil <alieander@gmail.com>
2014-01-30 12:12:16 -0800

    chruby and some aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,7 +1,8 @@
-alias r='rbenv local 1.8.7-p358'
-
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
 
 alias migrate='rake db:migrate db:test:clone'
+
+alias be='bundle exec'
+alias r='chruby'
