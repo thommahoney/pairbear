bd506ecfde67a70c2b0f5042a4821b8d482313e1
Pairbear <pairbear@dragon.local>
2015-07-02 15:31:05 -0700

    zsh configuration stuffs

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -15,40 +15,37 @@ HISTFILE=~/.zsh_history
 HISTSIZE=100000
 SAVEHIST=100000
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
+##
+# http://linux.die.net/man/1/zshoptions
+#
 setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
+setopt CORRECT
+setopt LOCAL_OPTIONS    # allow functions to have local options
+setopt LOCAL_TRAPS      # allow functions to have local traps
+setopt NO_LIST_BEEP
+setopt PROMPT_SUBST
 
-setopt APPEND_HISTORY # adds history
-#setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt APPEND_HISTORY       # adds history
+setopt EXTENDED_HISTORY     # add timestamps to history
+setopt HIST_IGNORE_ALL_DUPS # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY          # don't execute history right away, instead set prompt to command
+setopt INC_APPEND_HISTORY   # adds history incrementally and share it across sessions
+setopt SHARE_HISTORY        # share history between sessions
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+setopt COMPLETE_ALIASES
 
-zle -N newtab
+#zle -N newtab # unknown
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
-
-bindkey -v
+#bindkey '^[^[[D' backward-word
+#bindkey '^[^[[C' forward-word
+#bindkey '^[[5D' beginning-of-line
+#bindkey '^[[5C' end-of-line
+#bindkey '^[[3~' delete-char
 #bindkey '\e[3~' delete-char
-bindkey '^R' history-incremental-search-backward
+#bindkey '^[^N' newtab
+#bindkey '^?' backward-delete-char
 
-#set -o vi
+bindkey -e # emacs bindings
