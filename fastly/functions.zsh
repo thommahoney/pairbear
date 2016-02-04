b225e7af169d6e66de45a6207c3e36de7e6ef635
Tommy O'Neil <vagrant@fastlydev>
2016-02-04 00:48:01 +0000

    None of this is valid anymore...
    The new dev env has different practices

--- fastly/functions.zsh
+++ fastly/functions.zsh
@@ -1,33 +1,3 @@
-function fastly_route {
+function fastly_routes {
 	sudo route add 10.215.0.0/24 fastlydev.local
-}
-
-function fusion {
-  /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion $1 ~/Documents/fastlydev.vmwarevm nogui
-}
-
-function fusion2 {
-  /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion $1 ~/Documents/fastlydev2.vmwarevm nogui
-}
-
-function morning {
-	fusion start
-	echo 'sleeping 15'
-	sleep 5
-	echo 'sleeping 10'
-	sleep 5
-	echo 'sleeping 5'
-	sleep 5
-	fastly_route
-}
-
-function morning2 {
-	fusion2 start
-	echo 'sleeping 15'
-	sleep 5
-	echo 'sleeping 10'
-	sleep 5
-	echo 'sleeping 5'
-	sleep 5
-	fastly_route
 }
